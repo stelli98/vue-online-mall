959a92dcb325debe441c50627685ed5458319281
stelli98_ <stellitan98@gmail.com>
2019-03-08 13:55:26 +0700

    V.1.0 Vue Introduction Online Mall

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <router-link to="/contact-us">Contact Us</router-link>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.content {
+  padding-top: 20px;
+}
+</style>
---
+++ src/views/ContactUsPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>
+      <label>Full Name</label>
+      <input type="text" class="double-input" v-model="formData.fullName">
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="formData.email">
+    </div>
+    <div>
+      <label>Description</label>
+      <textarea name id cols="30" rows="10" v-model="formData.description"></textarea>
+    </div>
+    <div>
+      <button @click="submitForm">Submit</button>
+    </div>
+    <div>
+      <p>{{formData}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactUs',
+  data () {
+    return {
+      formData: {
+        fullName: '',
+        email: '',
+        description: ''
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      alert('Data submitted')
+    }
+  }
+}
+</script>
+
+<style scoped>
+label {
+  display: block;
+  margin: 20px;
+}
+</style>
